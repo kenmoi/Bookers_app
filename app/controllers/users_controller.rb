6e79f4b7deb289e7fe719205cc96900a56e7b470
kenmoi <ken.en001.mb@gmail.com>
2021-06-11 22:15:45 +0000

    [Add] practice-feature-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,34 @@
 class UsersController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @users = @user
+    @user_all = User.all
+    @book = Book.new
   end
   
   def create
   end
   
+  def show
+  end
+  
   def edit
+     @user = User.find(params[:id])
+     if current_user == @user
+       @user = User.find(params[:id])
+     else
+       redirect_to user_path(current_user.id)
+     end
   end
   
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
   
   def destroy
