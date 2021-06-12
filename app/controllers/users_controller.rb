de4316ca3e8ba652d75a6e470d781128367a91c3
kenmoi <ken.en001.mb@gmail.com>
2021-06-12 11:58:32 +0000

    [Add] practice-feature-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,14 @@ class UsersController < ApplicationController
     @user_all = User.all
     @book = Book.new
   end
-  
-  def create
-  end
-  
+
   def show
+    @user = User.find(params[:id])
+    @users = @user
+    @books = @user.books
+    @book = Book.new
   end
-  
+
   def edit
      @user = User.find(params[:id])
      if current_user == @user
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
        redirect_to user_path(current_user.id)
      end
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -30,14 +31,13 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name)
   end
 
-  
 end
