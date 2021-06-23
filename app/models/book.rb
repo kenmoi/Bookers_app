a741df4fbfdf48f23924790bda4abf409383852c
kenmoi <ken.en001.mb@gmail.com>
2021-06-23 19:56:03 +0000

    [Add] practice-favorite

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
     validates :title,:body, presence: true
     has_many :favorites, dependent: :destroy
+    
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,26 @@ class User < ApplicationRecord
          
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :followers, through: :follower_relationships, source: :followed
+  has_many :followed_Relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followeds, through: :followed_Relationships, source: :follower
+
+  
+  def following?(other_user)
+    self.followers.include?(other_user)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    unless self == other_user
+    self.follower_relationships.create(followed_id: other_user.id)
+    end
+  end
+  
+  def unfollow(other_user)
+    self.follower_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
          
 end
